6881daeb05d620ffff89b9103ae00899330fda7d
Tegacreatives <contact@tegacreatives.com>
2023-10-13 17:23:01 +0100

    Removed unused imports

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,5 @@
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
-import { HEADERHEIGHT } from "../../constants";
-import { theme } from "../../theme";
 import ExploreHeader from "../components/ExploreHeader";
 import UpcomingEvents from "../components/UpcomingEvents";
 import Ad from "../components/Ad";

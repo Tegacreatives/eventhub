dd598a9bae383c10f4092a993852dcb160225f8a
Tegacreatives <contact@tegacreatives.com>
2023-10-23 23:39:13 +0100

    Added Cards to Nearby you components

--- src/components/NearByEvents.tsx
+++ src/components/NearByEvents.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import SectionTitle from "./SectionTitle";
+import EventCard from "./EventCard";
 
 const NearByEvents = () => {
   return (
     <View>
       <SectionTitle title="Nearby You" />
+      <EventCard />
     </View>
   );
 };
